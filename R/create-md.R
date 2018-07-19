571f04eb2969c4fdd51a9504ec5f4e56a708d9b5
Yihui Xie <xie@yihui.name>
2018-07-19 15:12:53 -0500

    use some base R since code of the tidyverse style is fairly inconvenient for interactive execution or debugging (intermediate results are not saved, so you must be smart enough to write the correct code at the first time)
    the main things I did in the enourmous changes are:
    
    1. calculate toc_len;
    2. try not to exclude certain urls by hand (exclude.txt) but automatically filter them out later (by toc length and age)

--- R/create-md.R
+++ R/create-md.R
@@ -3,8 +3,8 @@ xfun::pkg_attach2(c('purrr', 'dplyr', 'xml2'))
 # Book listing ------------------------------------------------------------
 
 # get book from sitemap
-book_list <- xml2::as_list(read_xml("https://bookdown.org/sitemap.xml"))[[1]]
-book_urls <- tibble(
+book_list = xml2::as_list(read_xml("https://bookdown.org/sitemap.xml"))[[1]]
+book_urls = tibble(
   url = map_chr(book_list, list("loc", 1)),
   lastmod = map_chr(book_list, list("lastmod", 1)),
   from = "bookdown.org") %>%
@@ -25,103 +25,96 @@ book_urls <- tibble(
 
 # one xml_find for two use case
 xml_find = function(x, xpath, all = FALSE) {
-  FUN <- if (all) xml_find_all else xml_find_first
+  FUN = if (all) xml_find_all else xml_find_first
   tryCatch(FUN(x, xpath), error = function(e) NULL)
 }
-
-# check if covers url is accessible or do not use it
-check_cover <- function(cover, url) {
-  # relative URL to absolute
-  if (!grepl('^https?://', cover)) cover <- paste0(url, cover)
-  # is the cover image URL accessible?
-  if (tryCatch(httr::http_error(cover), error = function(e) TRUE)) cover = NA_character_
-  cover
-}
-
-# get list of authors from url or html content
-clean_authors <- function(author, url) {
-  if (is.null(author) || length(author) == 0) {
-    author = unlist(strsplit(url, '/'))  # https://bookdown.org/user/book
-    author = author[length(author) - 1]
-  } else {
-    author = xml_attr(author, 'content')
-    author = paste(author, collapse = ', ')
-  }
-  author
-}
-
-
 
 
 # Get books meta ----------------------------------------------------------
 
 # get metadata for a book from html content
-get_book_meta <- function(url, date) {
-  tibble(
-    url = url,
-    html = url %>%
-      possibly(~ read_html(.x, encoding = "UTF-8") %>% list(), otherwise = NA_character_)(),
-    title = map_chr(html,
-                    ~ xml_find(.x, ".//title") %>%
-                      possibly(xml_text, otherwise = NA_character_)()),
-    date = if_else(is.na(date),
-                   map_chr(html,
-                           ~ xml_find(.x, './/meta[@name="date"]') %>%
-                             possibly(~ xml_attr(.x, "content"), otherwise = NA_character_)()
-                   ),
-                   date),
-    description = map_chr(html,
-                          ~ xml_find(.x, './/meta[@name="description"]') %>%
-                            possibly(~ xml_attr(.x, "content"), otherwise = NA_character_)()),
-    cover = map_chr(html,
-                    ~ xml_find(.x, './/meta[@property="og:image"]') %>%
-                      possibly(~ xml_attr(.x, "content"), otherwise = NA_character_)() %>%
-                      check_cover(url = url)),
-    repo = map_chr(html,
-                   ~ xml_find(.x, './/meta[@name="github-repo"]') %>%
-                     possibly(~ xml_attr(.x, "content"), otherwise = NA_character_)()),
-    authors = map_chr(html,
-                      ~ xml_find(.x, './/meta[@name="author"]', all = TRUE) %>%
-                        clean_authors(url = url)),
-    # get generator to identify
-    generator = map_chr(html,
-                        ~ xml_find(.x, './/meta[@name="generator"]') %>%
-                          possibly(~ xml_attr(.x, "content"), otherwise = NA_character_)()),
-    toc_len = if (grepl('gitbook', generator, ignore.case = TRUE)) {
-      length(xml_find(html[[1]], './/li[@class="chapter"]', TRUE))
-    } else NA_integer_
+get_book_meta = function(url, date) {
+  html = try(read_html(url, encoding = 'UTF-8'))
+  if (inherits(html, 'try-error')) return()
+
+  title = xml_find(html, ".//title")
+  if (length(title) == 0) return()
+  title = xml_text(title)
+  if (title == '') return()
+
+  description = xml_find(html, './/meta[@name="description"]')
+  if (is.null(description)) return()
+  description = xml_attr(description, 'content')
+  if (is.na(description) || description == 'NA') return()
+
+  author = xml_find(html, './/meta[@name="author"]', all = TRUE)
+  if (length(author) == 0) {
+    author = unlist(strsplit(url, '/'))  # https://bookdown.org/user/book
+    author = author[length(author) - 1]
+  } else {
+    author = xml_attr(author, 'content')
+    author = paste(author, collapse = ', ')
+    # bookdown-demo published by other people
+    if (title == 'A Minimal Book Example' && author == 'Yihui Xie' && !grepl('/yihui/', url))
+      return()
+  }
+
+  if (is.na(date)) {
+    date = xml_find(html, './/meta[@name="date"]')
+    date = if (is.null(date)) NA else {
+      date = xml_attr(date, 'content')
+      # is it a valid date?
+      if (inherits(xfun::try_silent(as.Date(date)), 'try-error')) NA else date
+    }
+  }
+
+  cover = xml_find(html, './/meta[@property="og:image"]')
+  if (!is.null(cover)) {
+    cover = xml_attr(cover, 'content')
+    # relative URL to absolute
+    if (!grepl('^https?://', cover)) cover = paste0(url, cover)
+    # is the cover image URL accessible?
+    if (tryCatch(httr::http_error(cover), error = function(e) TRUE)) cover = NULL
+  }
+
+  repo = xml_find(html, './/meta[@name="github-repo"]')
+  if (!is.null(repo)) repo = xml_attr(repo, 'content')
+  generator = xml_find(html, './/meta[@name="generator"]')
+  generator = if (length(generator)) xml_attr(generator, "content") else NA
+  toc_len = if (grepl('gitbook', generator, ignore.case = TRUE)) {
+    length(xml_find(html, './/li[@class="chapter"]', TRUE))
+  } else NA
+
+  data_frame(
+    url = url, title = title, authors = author, date = date, description = description,
+    cover = if (is.null(cover)) NA else cover,
+    repo = if (is.null(repo)) NA else repo, toc_len = toc_len
   )
 }
 
-# delete book parsed list
-unlink("listed.txt")
-cache_rds <- "_book_meta.rds"
-books_metas <- book_urls %>%
+cache_rds = "_book_meta.rds"
+books_metas = book_urls %>%
   # exclude some specific books
   filter(! url %in% readLines("exclude.txt")) %>%
   # exclude all bookdown demo except official one
   filter(! (grepl('/bookdown-demo/$', url) & !grepl('/yihui/', url))) %>%
-  # exclude all book from one author
-  filter(!grepl('^https://bookdown.org/ChaitaTest/', url)) %>%
-  # slice(1:2) %>%
   select(url, lastmod) %>%
   pmap_df( ~ {
-    url <- .x
+    url = .x
     # pmap strips dates so they need to be character
     # https://github.com/tidyverse/purrr/issues/358
-    date <- .y
+    date = .y
     message("processing ", url, " ### ", appendLF = FALSE)
-    cat(url, sep = '\n', file = 'listed.txt', append = TRUE)
     if (file.exists(cache_rds)) {
-      book_metas <- readRDS(cache_rds)
-      if (!is.na(date) && identical(book_metas[[url]][['date']], date)) {
+      book_metas = readRDS(cache_rds)
+      if (!is.na(date) && identical((book_meta <- book_metas[[url]])[['date']], date)) {
         message("(from cache)")
-        return(book_metas[[url]])
+        return(if (!is.null(book_meta[['title']])) book_meta)
       }
-    } else book_metas <- list()
+    } else book_metas = list()
     message("(from url)")
-    book_meta <- get_book_meta(url, date)
-    book_metas[[url]] <- book_meta
+    book_meta = get_book_meta(url, date)
+    book_metas[[url]] = if (is.null(book_meta)) list(date = date) else book_meta
     saveRDS(book_metas, cache_rds)
     book_meta
   })
@@ -129,49 +122,37 @@ books_metas <- book_urls %>%
 
 # Cleaning published books ------------------------------------------------
 
-pinned_urls <- readLines("home.txt")
-
-books_to_keep <- books_metas %>%
-  # do not keep non accessible book
-  filter(! is.na(html)) %>%
-  # do not keep publication with no title
-  filter(! is.na(title)) %>%
-  # do not keep publication with no description
-  filter(! is.na(description)) %>%
-  # do not keep template book
-  filter(!(title == 'A Minimal Book Example' & authors == 'Yihui Xie' & !grepl('/yihui/', url))) %>%
-  # mark pinned url
-  mutate(pinned = tolower(url %in% pinned_urls))
+books_to_keep = books_metas %>%
+  # mark pinned url (to be displayed on homepage)
+  mutate(pinned = tolower(url %in% readLines("home.txt")))
 
 
 # render_post -------------------------------------------------------------
 
-make_post_filename <- function(url) {
-  name <- gsub("^http[s]?://|/$", "", tolower(url))
-  name <- gsub("[^a-z0-9]+", "-", name)
-  name <- gsub("--+", "-", name)
-  i <- grepl('^bookdown-org-', name)
-  name[i]  <- sub('^bookdown-org-', 'internal/', name[i])
-  name[!i] <- file.path('external', name[!i])
+make_post_filename = function(url) {
+  name = gsub("^http[s]?://|/$", "", tolower(url))
+  name = gsub("[^a-z0-9]+", "-", name)
+  name = gsub("--+", "-", name)
+  i = grepl('^bookdown-org-', name)
+  name[i]  = sub('^bookdown-org-', 'internal/', name[i])
+  name[!i] = file.path('external', name[!i])
   paste0(name, ".md")
 }
 
-write_md_post <- function(post_name, post_content, path = "../content/archive") {
+write_md_post = function(post_name, post_content, path = "../content/archive") {
   xfun::write_utf8(post_content, file.path(path, post_name))
 }
 
-template <- xfun::read_utf8("template.md")
+template = xfun::read_utf8("template.md")
 
 xfun::in_dir('../content/archive', unlink(c('internal/*.md', 'external/*.md')))
 
 books_to_keep %>%
-  mutate(post_content = pmap_chr(.,
-                             function(...) {
-                               ldata <- list(...)
-                               ldata <- ldata[!is.na(ldata)]
-                               whisker::whisker.render(template, data = ldata)
-                             })
-  ) %>%
+  mutate(post_content = pmap_chr(., function(...) {
+    ldata = list(...)
+    ldata = ldata[!is.na(ldata)]
+    whisker::whisker.render(template, data = ldata)
+  })) %>%
   select(url, post_content) %>%
   mutate(post_name = make_post_filename(url)) %>%
   select(-url) %>%
