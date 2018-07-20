769c72d9f88bd6182fb6cb4058ec5449c8330142
Yihui Xie <xie@yihui.name>
2018-07-20 13:28:57 -0500

    deploy the site on my own computer

--- R/build.R
+++ R/build.R
@@ -1,1 +1,6 @@
 xfun::in_dir('R', sys.source('create-md.R', environment()))
+
+if ('yihui' %in% rsconnect::accounts('bookdown.org')[['name']]) {
+  blogdown::hugo_build()
+  rsconnect::deploySite('.', 'homepage', 'yihui', 'bookdown.org')
+}
