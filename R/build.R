b41d3e5ec7e000096012fdd132f26c2086b7a684
Yihui Xie <xie@yihui.name>
2018-07-20 16:38:16 -0500

    make sure the wd is correct before publishing the book

--- R/build.R
+++ R/build.R
@@ -1,6 +1,7 @@
-setwd(blogdown:::site_root())
+setwd(wd <- blogdown:::site_root())
 xfun::in_dir('.', sys.source('R/create-md.R', environment()))
 
+setwd(wd)
 if ('yihui' %in% rsconnect::accounts('bookdown.org')[['name']]) {
   blogdown::hugo_build()
   rsconnect::deploySite('.', 'homepage', 'yihui', 'bookdown.org')
