0ad54d216a210630bd7fe7d3105a15cfeaed00f4
Yihui Xie <xie@yihui.name>
2018-07-20 13:24:23 -0500

    convert relative links to absolute ones before replacing state, otherwise these relative links will be relative to / (should be relative to /home/)

--- static/js/fix-url.js
+++ static/js/fix-url.js
@@ -2,6 +2,13 @@
 // bookdown.org's real home is at bookdown.org/home/, which is an ugly URL
 (function() {
   var loc = window.top.location, his = window.top.history;
-  if (loc.protocol === 'https:' && loc.pathname !== '/' && his.replaceState)
-    his.replaceState({}, '', '/');
+  if (loc.protocol !== 'https:' || /^\/(connect\/)?$/.test(loc.pathname) || !his.replaceState) return;
+  // resolve relative links to absolute links, otherwise they'll be relative to /
+  // after we replace state
+  var i, links = document.querySelectorAll('a');
+  for (i = 0; i < links.length; i++) {
+    if (/^https?:\/\//.test(links[i].getAttribute('href'))) continue;
+    links[i].href = links[i].href;
+  }
+  his.replaceState({}, '', '/');
 })();
