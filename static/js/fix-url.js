f927fff359885c8c90aaf8b20d6ed5ff5212f45a
Yihui Xie <xie@yihui.name>
2018-07-23 10:45:20 -0500

    only "redirect" the homepage from /home/ to /
    cc @jeroenjanssens

--- static/js/fix-url.js
+++ static/js/fix-url.js
@@ -2,7 +2,7 @@
 // bookdown.org's real home is at bookdown.org/home/, which is an ugly URL
 (function() {
   var loc = window.top.location, his = window.top.history;
-  if (loc.protocol !== 'https:' || /^\/(connect\/)?$/.test(loc.pathname) || !his.replaceState) return;
+  if (loc.pathname !== '/home/' || !his.replaceState) return;
   // resolve relative links to absolute links, otherwise they'll be relative to /
   // after we replace state
   var i, links = document.querySelectorAll('a');
