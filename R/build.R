d7b27c9dfcef2fdb72503e5695c2ff8ea1268b0e
Yihui Xie <xie@yihui.name>
2020-11-01 21:21:12 -0600

    don't run create-md.R when previewing the site locally

--- R/build.R
+++ R/build.R
@@ -1,1 +1,2 @@
-xfun::in_dir(blogdown:::site_root(), sys.source('R/create-md.R', environment()))
+if (identical(commandArgs(TRUE), 'FALSE'))
+  xfun::in_dir(blogdown:::site_root(), sys.source('R/create-md.R', environment()))
