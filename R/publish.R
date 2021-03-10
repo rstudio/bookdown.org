9f3d03ef84095bad85ea2a14acf381309531696d
christophe dervieux <christophe.dervieux@gmail.com>
2021-03-10 17:25:44 +0100

    Add build and publish workflow on CI

--- R/publish.R
+++ R/publish.R
@@ -1,5 +1,28 @@
-xfun::in_dir((blogdown:::site_root()), if ('yihui' %in% rsconnect::accounts('bookdown.org')[['name']]) {
-  blogdown::hugo_build()
-  options(rsconnect.force.update.apps = TRUE)
-  rsconnect::deploySite('.', 'homepage', 'yihui', 'bookdown.org', launch.browser = FALSE)
-})
+xfun::in_dir(
+  (blogdown:::site_root()), 
+  {
+    # BUILD SITE
+    blogdown::hugo_build()
+    
+    # DEPLOY
+    if (Sys.getenv("CI") == "true") {
+      rsconnect::addConnectServer('https://bookdown.org', 'bookdown.org')
+      rsconnect::connectApiUser(
+        account = 'GHA', server = 'bookdown.org',
+        apiKey = Sys.getenv('RSC_BOOKDOWN_ORG_TOKEN')
+      )
+      rsconnect::deploySite(
+        appId = Sys.getenv('CONTENT_ID'),
+        server = 'bookdown.org',
+        render = 'none', logLevel = 'verbose',
+        forceUpdate = TRUE
+      )
+    } else {
+      # for local deployment 
+      rsconnect::deploySite('.', siteName = 'homepage', 
+                            server = 'bookdown.org', 
+                            forceUpdate = TRUE,
+                            launch.browser = FALSE)
+    }
+  }
+)
